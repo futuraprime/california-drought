9da8001e55056f6ab9636d14d3b56a5a7088db3a
Evan Hensleigh <futuraprime@gmail.com>
2015-02-16 23:07:18 -0500

    Well, only if we want it to render next century...

--- js/main.js
+++ js/main.js
@@ -7,6 +7,20 @@ var chart = d3.select('#reservoirs');
 var things = chart.selectAll('circle.thing');
 var thingsdata = [];
 
+// month 0 is going to be January 1916 (for now)
+var startYear = 1916;
+var monthsScale = d3.scale.linear()
+  .domain([0,100 * 12])
+  .range([100,940]);
+
+function dateNumberToMonth(dateNumber) {
+  var dateString = dateNumber.toString();
+  var year = parseInt(dateNumber.substr(0,4), 10);
+  var month = parseInt(dateNumber.substr(4,2), 10) - 1;
+
+  return (year - startYear) * 12 + month;
+}
+
 var mainFsm = new machina.Fsm({
   initialize : function() {
     _.bindAll(this, 'addPoint');
@@ -22,19 +36,18 @@ var mainFsm = new machina.Fsm({
     console.log('loading data');
     this.loader = oboe('./data/station_data.json');
     this.loader.node('stations.*{storage}', this.addPoint);
-    this.loader.done(function() {
-      console.log("done!", self.data.length);
-      console.log(document.querySelectorAll('.reservoir').length);
-    });
   },
   addPoint : function(datum) {
     this.data.push(datum);
 
     this.binding = chart.selectAll('g.reservoir').data(this.data);
+    this.latitudeSort = _.sortBy(this.data, function(n) { return -n.Latitude; });
 
     this.render();
   },
   render : function() {
+    var self = this;
+
     var goneGroups = this.binding.exit().remove();
 
     var newGroups = this.binding.enter().append('svg:g')
@@ -42,11 +55,27 @@ var mainFsm = new machina.Fsm({
 
     newGroups.append('svg:text')
       .classed('reservoir-name', true)
+      .attr('y', 15)
       .text(function(d) { return d['Station Name']; });
 
+    newGroups.each(function(d) {
+      console.log(d.storage);
+      var group = d3.select(this);
+      var selection = group.selectAll('rect.month-block').data(d.storage);
+
+      // selection.enter().append('svg:rect')
+      //   .classed('month-block', true)
+      //   .attr('height', 15)
+      //   .attr('width', monthsScale(1))
+      //   .attr('x', function(i) {
+      //     // console.log(i.date);
+      //     return dateNumberToMonth(i.date);
+      //   });
+    });
+
     this.binding
-      .attr('transform', function(d, i) {
-        return getTransformString(0, i*20);
+      .attr('transform', function(d) {
+        return getTransformString(0, self.latitudeSort.indexOf(d)*20);
       });
 
     this.chart.attr('height', this.data.length * 20);
