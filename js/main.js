0a05513ccfec4427b402d94492bf218d4c2168d1
Evan Hensleigh <futuraprime@gmail.com>
2015-02-16 22:06:10 -0500

    Showing something useful, hurrah.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,54 @@
+function getTransformString(x, y, css) {
+  return 'translate('+(x || 0)+(css ? 'px' : '')+','+(y || 0)+(css ? 'px' : '')+')';
+}
+
 var chart = d3.select('#reservoirs');
 
 var things = chart.selectAll('circle.thing');
 var thingsdata = [];
 
-var xScale = d3.scale.linear()
-  .domain([-150,-100])
-  .range([100,300]);
-var yScale = d3.scale.linear()
-  .domain([20,40])
-  .range([250,50]);
+var mainFsm = new machina.Fsm({
+  initialize : function() {
+    _.bindAll(this, 'addPoint');
 
-oboe('./data/station_data.json')
-  .node('stations.*{storage}', function(thing) {
-    thingsdata.push(thing);
+    this.chart = d3.select('#reservoirs');
 
-    var dt = things.data(thingsdata);
+    this.data = [];
 
-    dt.enter().append('svg:circle')
-      .classed('thing', true)
-      .attr('cx', function(d) { return xScale(d.Longitude); })
-      .attr('cy', function(d) { return yScale(d.Latitude); })
-      .attr('r', 1);
-  });
+    this.loadData();
+  },
+  loadData : function() {
+    var self=this;
+    console.log('loading data');
+    this.loader = oboe('./data/station_data.json');
+    this.loader.node('stations.*{storage}', this.addPoint);
+    this.loader.done(function() {
+      console.log("done!", self.data.length);
+      console.log(document.querySelectorAll('.reservoir').length);
+    });
+  },
+  addPoint : function(datum) {
+    this.data.push(datum);
+
+    this.binding = chart.selectAll('g.reservoir').data(this.data);
+
+    this.render();
+  },
+  render : function() {
+    var goneGroups = this.binding.exit().remove();
+
+    var newGroups = this.binding.enter().append('svg:g')
+      .classed('reservoir', true);
+
+    newGroups.append('svg:text')
+      .classed('reservoir-name', true)
+      .text(function(d) { return d['Station Name']; });
+
+    this.binding
+      .attr('transform', function(d, i) {
+        return getTransformString(0, i*20);
+      });
+
+    this.chart.attr('height', this.data.length * 20);
+  }
+});
