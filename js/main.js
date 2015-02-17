d4139d6db09dff9e9f9fb81344c2569412027f69
Evan Hensleigh <futuraprime@gmail.com>
2015-02-16 23:35:38 -0500

    Well, it's a start... not too bad, actually.

--- js/main.js
+++ js/main.js
@@ -12,6 +12,12 @@ var startYear = 1916;
 var monthsScale = d3.scale.linear()
   .domain([0,100 * 12])
   .range([100,940]);
+var yearsScale = d3.scale.linear()
+  .domain([startYear, startYear + 100])
+  .range([100,940]);
+var fillScale = d3.scale.linear()
+  .domain([0,1])
+  .range([0,15]);
 
 function dateNumberToMonth(dateNumber) {
   var dateString = dateNumber.toString();
@@ -38,6 +44,20 @@ var mainFsm = new machina.Fsm({
     this.loader.node('stations.*{storage}', this.addPoint);
   },
   addPoint : function(datum) {
+    var yearValues = {};
+    _.each(datum.storage, function(i) {
+      var year = i.date.toString().substr(0,4);
+      if(!yearValues[year]) { yearValues[year] = []; }
+      // we're guarding against those 'm' values here...
+      if(typeof i.value === 'number') { yearValues[year].push(i.value); }
+    });
+    datum.years = _.map(yearValues, function(v,k) {
+      return {
+        year : k,
+        value : _.reduce(v, function(memo, value) { return memo + value; }) / v.length
+      };
+    });
+
     this.data.push(datum);
 
     this.binding = chart.selectAll('g.reservoir').data(this.data);
@@ -59,18 +79,24 @@ var mainFsm = new machina.Fsm({
       .text(function(d) { return d['Station Name']; });
 
     newGroups.each(function(d) {
-      console.log(d.storage);
+      console.log(d);
       var group = d3.select(this);
-      var selection = group.selectAll('rect.month-block').data(d.storage);
+      var selection = group.selectAll('rect.month-block').data(d.years);
+      // fix this in python...
+      var capacity = parseInt(d['Capacity (AF)'].split(',').join(''), 10);
 
-      // selection.enter().append('svg:rect')
-      //   .classed('month-block', true)
-      //   .attr('height', 15)
-      //   .attr('width', monthsScale(1))
-      //   .attr('x', function(i) {
-      //     // console.log(i.date);
-      //     return dateNumberToMonth(i.date);
-      //   });
+      selection.enter().append('svg:rect')
+        .classed('month-block', true)
+        .attr('height', function(y) {
+          return fillScale(y.value/capacity);
+        })
+        .attr('y', function(y) {
+          return 15 - fillScale(y.value/capacity);
+        })
+        .attr('width', yearsScale(1) - yearsScale(0))
+        .attr('x', function(y) {
+          return yearsScale(y.year);
+        });
     });
 
     this.binding
